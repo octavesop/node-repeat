f7780a548e28fb1d14abf06737aee9fc820a132b
Mina Jung <octavesop@gmail.com>
2021-02-16 22:14:13 +0900

    파일을 읽어와서 본문 구현하기
    제곧내

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var app = http.createServer(function(request, response) {
+ 
+var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    console.log(queryData.id);
-    console.log(_url);
-
-    if(_url == '/') {
-        title = 'Welcome!';
+    if(_url == '/'){
+        //_url = '/index.html'; //이렇게 하면 index.html로 이동하지렁
+        title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
-        return response.writeHead(404);
+      return response.writeHead(404);
     }
     response.writeHead(200);
-
-    var template = 
-    `
-            <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>${title}</title>
-        </head>
-        <body>
-            <h1>${title}</h1>
-            <ol>
-                <li><a href = "/?id=HTML">HTML</a></li>
-                <li><a href = "/?id=CSS">CSS</a></li>
-                <li><a href = "/?id=JS">JS</a></li>
-            </ol>
-        </body>
-        </html>
-    `;
-
-    response.end(template);
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JS">JS</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
+ 
+ 
 });
 app.listen(3000);
