2f1ae12b02efcb5e8afca14811d2c1440d4f7461
Mina Jung <octavesop@gmail.com>
2021-02-21 23:25:41 +0900

    Update main.js
    오늘은 글 목록을 출력하는 방법을 디렉토리를 통해 사용해 봤음.

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const { mainModule } = require('process');
  
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
+
+        fs.readdir('./data', function(error, fileList){
+          console.log(fileList);
+          
+
+          /*
+          var list = `
+          <ul>
+              {<li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JS">JS</a></li>}
+            </ul>`;
+            */
+          var list = '<ul>';
+          var i = 0;
+          while(i< fileList.length) {
+            list = list + `<li><a href = "/?id=${fileList[i]}">${fileList[i]}</li>`;
+            i = i+1;
+          }
           var template = `
           <!doctype html>
           <html>
@@ -20,19 +40,18 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JS">JS</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
           </html>
           `;
+          
           response.writeHead(200);
           response.end(template);
-        });
+          list = list + '</ul>';
+        })
+        
       } else {
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
